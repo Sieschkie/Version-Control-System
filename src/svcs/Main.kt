8efba1e176daf9ea471a01f2123c8324bd780324
Laufer Svitlana <laufersvitlana@gmail.com>
2024-04-15 18:18:48 +0200

    fun config done(set/get)

--- src/svcs/Main.kt
+++ src/svcs/Main.kt
@@ -1,5 +1,7 @@
 package svcs
 
+import java.io.File
+
 fun help() {
     println("These are SVCS commands:\n" +
             "config     Get and set a username.\n" +
@@ -8,11 +10,46 @@ fun help() {
             "commit     Save changes.\n" +
             "checkout   Restore a file.")
 }
+fun isValidInput(username: String): Boolean { //
+    val regex = Regex("^[a-zA-Z][a-zA-Z0-9]*$")
+    return username.isNotBlank() && username.length in 3..20 && regex.matches(username)
+}
 
-fun main(args: Array<String>) {
+fun config(name: String?) {
+    val workingDirectory = System.getProperty("user.dir")
+    val separator = File.separator
+    val absolutePath = "${workingDirectory}${separator}vcs"
+    val dir = File(absolutePath)
+    if (!dir.exists()) {
+        dir.mkdirs()
+    }
+    val configFile = dir.resolve("config.txt")
+    if (!configFile.exists()) {
+        try {
+            configFile.createNewFile()
+        } catch (_: Exception) {}
+    }
+    if (name !== null){
+        configFile.writeText(name)
+    } else {
+        val configValue = configFile.readText()
+        if(configValue.isEmpty()) {
+            println("Please, tell me who you are.")
+            val username = readLine()?.trim() ?: ""
+            if(isValidInput(username)) {
+                configFile.writeText(username)
+            }
+        }
+        println("The username is $configValue")
+    }
+}
+
+//fun main(args: Array<String>)
+fun main() {
+    val args = readln().split(" ") //test
     when(args.firstOrNull()?.lowercase()?.trim()) {
         null, "--help" -> help()
-        "config" -> println("Get and set a username.")
+        "config" -> config(args.getOrNull(1)?.trim())
         "add" -> println("Add a file to the index.")
         "log" -> println("Show commit logs.")
         "commit" -> println("Save changes.")
